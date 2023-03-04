43da885dc09f50f4ab8c8957f83d7b287469b24c
Nino Walker <nino.walker@gmail.com>
2023-03-04 15:57:10 +0100

    New mac

--- OSX/env.zsh
+++ OSX/env.zsh
@@ -1,3 +1,9 @@
+if ! which brew > /dev/null; then
+    if [[ -e /opt/homebrew/bin/brew ]]; then
+        export PATH="/opt/homebrew/bin:$PATH"
+    fi
+fi
+
 if [[ ! -h /Library/Java/JavaVirtualMachines/openjdk.jdk ]] && [[ -d /usr/local/opt/openjdk/libexec/openjdk.jdk ]]; then
     ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 fi
